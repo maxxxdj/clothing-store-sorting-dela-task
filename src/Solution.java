17df34fc8916c8bdb4af0d50a7a1d9e8f1abeb94
Mrtin Rockinstone Miloshev <59373264+maxxxdj@users.noreply.github.com>
2023-10-02 03:08:56 +0300

    Update solution sorting

--- src/Solution.java
+++ src/Solution.java
@@ -4,20 +4,20 @@ import lombok.NoArgsConstructor;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
-
 public class Solution {
     enum Season{
         WINTER,
         SUMMER,
         SPRING,
         AUTUMN;
+
     }
     @Data
     @AllArgsConstructor
     @NoArgsConstructor
     public static class Product{
         AtomicLong id;
-        int price;
+        double price;
         boolean isHighlighted;
         Season season;
     }
@@ -25,19 +25,23 @@ public class Solution {
         final AtomicLong idGenerator = new AtomicLong(1);
 
         List<Product> productList = List.of(new Product(
-                new AtomicLong(idGenerator.getAndIncrement()),50,true, Season.WINTER),
-                new Product(new AtomicLong(idGenerator.getAndIncrement()),1,true, Season.SPRING),
-                new Product(new AtomicLong(idGenerator.getAndIncrement()),23,true, Season.WINTER),
-                new Product(new AtomicLong(idGenerator.getAndIncrement()),15,false, Season.WINTER),
-                new Product(new AtomicLong(idGenerator.getAndIncrement()),56,true, Season.SUMMER),
-                new Product(new AtomicLong(idGenerator.getAndIncrement()),12,true, Season.AUTUMN),
-                new Product(new AtomicLong(idGenerator.getAndIncrement()),40,true, Season.SUMMER));
+                        new AtomicLong(idGenerator.getAndIncrement()), 53.00, true, Season.WINTER),
+                new Product(new AtomicLong(idGenerator.getAndIncrement()), 14.00, true, Season.SPRING),
+                new Product(new AtomicLong(idGenerator.getAndIncrement()), 5.70, true, Season.WINTER),
+                new Product(new AtomicLong(idGenerator.getAndIncrement()), 15.00, false, Season.WINTER),
+                new Product(new AtomicLong(idGenerator.getAndIncrement()), 56.00, true, Season.SUMMER),
+                new Product(new AtomicLong(idGenerator.getAndIncrement()), 2.00, true, Season.AUTUMN),
+                new Product(new AtomicLong(idGenerator.getAndIncrement()), 40.00, true, Season.SUMMER));
 
-        List<Product> orderedProducts =
-        productList.stream()
-                .filter(el -> el.price > 10)
-                .sorted(Comparator.comparing(Product::getSeason, Comparator.comparing(Enum::toString)).reversed()
-                        .thenComparing(el -> !el.isHighlighted))
-                .toList();
+        /**Getting all the products sorted by season, then by highlighted.*/
+        final List<Product> orderedProducts =
+                productList.stream()
+                        .filter(el -> el.price > 9.99)
+                        .sorted(Comparator.comparing(Product::getSeason,
+                                        Comparator.comparing(el -> el.toString().equalsIgnoreCase("WINTER")))
+                                                .thenComparing(el->el.isHighlighted).reversed())
+                        .toList();
+
+        orderedProducts.forEach(System.out::println);
     }
 }
